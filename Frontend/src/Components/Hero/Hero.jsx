0c9657d205c4533036be0efb08f29b21da7d065f
mayank2904gupta <mayankgupta2904@gmail.com>
2025-04-16 18:14:06 +0530

    feat(hero): add animations to hero section

--- Frontend/src/Components/Hero/Hero.jsx
+++ Frontend/src/Components/Hero/Hero.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { motion } from "motion/react";
+import AuthContext from "../../Context/AuthContext";
 
 export default function Hero() {
+  const { userId } = useContext(AuthContext);
   return (
     <>
-      <div class="overflow-x-hidden">
+      <motion.div
+        class="overflow-x-hidden"
+        initial={{ opacity: 0.2, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
         <div class="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
           <div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="mx-auto max-w-3xl text-center">
-              <h1 class="mt-5 text-3xl font-light leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight">
+              <motion.h1
+                initial={{ opacity: 0.3 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+                class="mt-5 text-3xl font-light leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight"
+              >
                 Increase <br class="sm:hidden" />
                 Knowledge with
-                <span class="relative inline-flex justify-center whitespace-nowrap font-bold">
+                <motion.span
+                  class="relative inline-flex justify-center whitespace-nowrap font-bold"
+                  initial={{ opacity: 0.3, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.5, duration: 0.5 }}
+                >
                   <svg
                     class="absolute -bottom-8 hidden w-2/3 text-gray-400 sm:block"
                     viewBox="0 0 490 42"
@@ -26,23 +44,35 @@ export default function Hero() {
                       fill="currentColor"
                     />
                   </svg>
-             Xamgen
-                </span>
-              </h1>
+                  Xamgen
+                </motion.span>
+              </motion.h1>
               <p class="mx-auto mt-12 max-w-md leading-7 text-gray-600">
-               Yours one and only exam solution 
+                Yours one and only exam solution
               </p>
 
               <div class="group relative mt-10 inline-flex">
-                <a
-                  href="#"
+                <motion.a
+                  href={userId === null ? "/signup" : "/"}
+                  initial={{ scale: 0.5, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.85 }}
+                  transition={{
+                    default: { duration: 0.1 },
+                    opacity: { duration: 1, delay: 0.3 },
+                    scale: { duration: 0.4, delay: 0.3 },
+                  }}
                   title=""
                   class="rounded-xl bg-blue-700 px-10 py-3 font-medium text-white transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 hover:bg-blue-600"
                 >
-                  Get free trial
-                </a>
+                  Get Started
+                </motion.a>
                 <div class="-scale-x-100 absolute left-0 -bottom-10 hidden h-10 w-10 -rotate-12 text-blue-600 md:inline-flex">
-                  <svg
+                  <motion.svg
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ delay: 0.5, duration: 0.5 }}
                     viewBox="0 0 82 35"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -59,14 +89,19 @@ export default function Hero() {
                       d="M33.9279 29.9296C33.9687 30.0252 34.0103 30.1211 34.0512 30.2167L36.776 28.708C36.7189 28.6018 36.6613 28.4961 36.6041 28.3903C35.7123 28.9033 34.8197 29.4166 33.9279 29.9296ZM55.213 27.9357C55.2513 28.0076 55.2895 28.0795 55.3278 28.1513C56.8382 27.5018 58.3486 26.8518 59.8593 26.2019C59.8129 26.092 59.7661 25.9821 59.7197 25.8726C58.2175 26.5602 56.7153 27.2481 55.213 27.9357ZM40.7384 18.9565C40.5279 17.8215 40.3393 16.6815 40.0998 15.5525C39.952 14.8551 39.5106 14.6711 38.8099 14.825C36.6153 15.3082 34.9909 17.2686 34.7963 19.6189C34.584 22.1806 36.0472 23.7605 37.8517 25.1395C37.9927 25.2475 38.5155 25.0604 38.6986 24.8591C40.2045 23.1998 40.6396 21.163 40.7384 18.9565ZM47.8846 27.7513C53.9169 27.9699 58.9887 25.6539 63.5351 21.8258C68.7108 17.4677 72.7252 12.1435 76.2413 6.39113C77.3061 4.64903 78.3271 2.87833 79.4328 1.16371C79.7291 0.70344 80.2137 0.234515 80.706 0.0824723C81.0457 -0.0225277 81.5473 0.410268 81.9765 0.603333C81.8444 0.859247 81.7237 1.12306 81.5774 1.37032C81.1827 2.03645 80.7194 2.66758 80.3867 3.36306C79.3033 5.6264 78.3041 7.93113 77.1981 10.1824C76.4525 11.6998 75.639 13.1905 74.7457 14.6225C74.1814 15.5269 73.3694 16.269 72.7471 17.1414C71.7606 18.5237 70.9604 20.0611 69.8622 21.3395C68.1531 23.33 66.4111 25.3434 64.4139 27.0174C59.9989 30.718 54.9038 32.5263 49.0801 32.1605C46.3701 31.9904 43.6835 31.9283 41.122 30.8655C40.842 30.7492 40.3185 30.9333 40.0448 31.1527C37.2899 33.3656 34.1239 34.5277 30.6632 34.7456C28.0734 34.909 25.4198 35.1171 22.8828 34.7219C20.7546 34.3908 18.675 33.3742 16.7198 32.3694C14.9819 31.4756 13.3686 30.2773 11.8348 29.0418C9.65017 27.2812 8.09522 24.9795 7.06601 22.3556C6.91824 21.9789 7.17257 21.2819 7.46774 20.9267C7.79559 20.5315 8.26675 20.7212 8.80326 20.9647C10.4826 21.7271 11.1635 23.3172 12.0776 24.6916C13.809 27.2959 16.297 28.8333 19.144 29.6515C24.0015 31.0477 28.8342 30.4606 33.5239 28.7422C36.0572 27.8134 36.0323 27.708 34.1863 25.8643C31.7566 23.438 30.4122 20.5417 30.5982 17.0518C30.8143 13.0012 34.1347 10.1538 38.1338 10.4515C39.3892 10.5452 40.2439 11.3239 41.0648 12.1255C42.9294 13.9466 43.9712 16.2194 44.3347 18.7977C44.7112 21.4648 44.7806 24.1113 43.5286 26.6189C43.2264 27.2244 43.5171 27.489 44.1483 27.5187C45.3947 27.5778 46.6393 27.6719 47.8846 27.7513Z"
                       fill="currentColor"
                     />
-                  </svg>
+                  </motion.svg>
                 </div>
               </div>
             </div>
           </div>
 
           <div class="mt-16 mb-16 flex flex-col items-center justify-center divide-y divide-gray-300 sm:flex-row sm:divide-x sm:divide-y-0 md:mt-20">
-            <div class="flex max-w-xs space-x-2 px-4 py-4">
+            <motion.div
+              class="flex max-w-xs space-x-2 px-4 py-4"
+              initial={{ opacity: 0 ,y:-20}}
+              animate={{ opacity: 1 ,y:0}}
+              transition={{ delay: 1, duration: 0.5 }}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-12 w-12 text-blue-600"
@@ -81,11 +116,12 @@ export default function Hero() {
                   d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
                 />
               </svg>
-              <p class="text-gray-600">
-                Earn credit by uploading question
-              </p>
-            </div>
-            <div class="flex max-w-xs space-x-2 px-4 py-4">
+              <p class="text-gray-600 ml-2">Earn credits by sharing your question papers.</p>
+            </motion.div>
+            <motion.div class="flex max-w-xs space-x-2 px-4 py-4"
+            initial={{ opacity: 0 ,y:-20}}
+            animate={{ opacity: 1 ,y:0}}
+            transition={{ delay: 1.5, duration: 0.5 }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-12 w-12 text-blue-600"
@@ -100,11 +136,14 @@ export default function Hero() {
                   d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
                 />
               </svg>
-              <p class="text-gray-600">
-               Increase your knowledge with different question
+              <p class="text-gray-600 ml-2">
+                Expand your knowledge with a range of past exam papers.
               </p>
-            </div>
-            <div class="flex max-w-xs space-x-2 px-4 py-4">
+            </motion.div>
+            <motion.div class="flex max-w-xs space-x-2 px-4 py-4"
+            initial={{ opacity: 0 ,y:-20}}
+            animate={{ opacity: 1 ,y:0}}
+            transition={{ delay: 2, duration: 0.5 }}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-12 w-12 text-blue-600"
@@ -119,13 +158,11 @@ export default function Hero() {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              <p class="text-gray-600">
-               GEt 10000 credit with only Rupee 500 
-              </p>
-            </div>
+              <p class="text-gray-600 ml-2">Get 10,000 credits for just ₹500 – unlock answers instantly!</p>
+            </motion.div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
